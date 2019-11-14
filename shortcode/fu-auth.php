f643230d1b3579d78a08cc26acc7ed1f9fad581d
vannha <phongnhatk21@gmail.com>
2019-11-14 10:53:52 +0700

    commit -

--- shortcode/fu-auth.php
+++ shortcode/fu-auth.php
@@ -30,7 +30,55 @@ function fu_auth_func( $atts ) {
     
     return fsUser()->get_template_file__( 'auth_form', array( 'atts' => $atts ), '', 'flex-login' );
 }
+add_shortcode( 'fu_login', 'fu_login_func' );
+function fu_login_func( $atts ) {
+    extract( shortcode_atts( array(
+    'el_class' => 'something',
+    'color' => '#FFF'
+    ), $atts ) );
+   
+    if ( is_user_logged_in() ) {
+        return fsUser()->get_template_file__( 'logout', array( 'atts' => $atts ), '', 'flex-login' );
+    }
+    $atts = shortcode_atts(
+        array(
+            'id' => '',
+        ), $atts );
+    wp_enqueue_script( 'jquery.validate.js', fsUser()->plugin_url . 'assets/vendor/jquery.validate.js', array(), '', true );
+    wp_register_script( 'fs-login.js', fsUser()->plugin_url . 'assets/js/fs-login.js', array(), '', true );
+    wp_localize_script( 'fs-login.js', 'fs_login', array(
+        'action' => 'fs_login',
+        'url'    => admin_url( 'admin-ajax.php' ),
+    ) );
+    wp_enqueue_script( 'fs-login.js' );
+    
+    return fsUser()->get_template_file__( 'login_form', array( 'atts' => $atts ), '', 'flex-login' );
+}
+add_shortcode( 'fu_register', 'fu_register_func' );
+function fu_register_func( $atts ) {
+    extract( shortcode_atts( array(
+    'el_class' => 'something',
+    'color' => '#FFF'
+    ), $atts ) );
+   
+    if ( is_user_logged_in() ) {
 
+        return fsUser()->get_template_file__( 'logout', array( 'atts' => $atts ), '', 'flex-login' );
+        
+    }
+    $atts = shortcode_atts(
+        array(
+            'id' => '',
+        ), $atts );
+    wp_enqueue_script( 'jquery.validate.js', fsUser()->plugin_url . 'assets/vendor/jquery.validate.js', array(), '', true );
+    wp_enqueue_script( 'fs-register.js', fsUser()->plugin_url . 'assets/js/fs-register.js', array(), '', true );
+    wp_localize_script( 'fs-register.js', 'fs_register', array(
+        'action' => 'fs_register',
+        'url'    => admin_url( 'admin-ajax.php' ),
+    ) );
+    
+    return fsUser()->get_template_file__( 'register_form', array( 'atts' => $atts ), '', 'flex-login' );
+}
 add_action('vc_before_init', 'add_flex_user_shortcodes');
 function add_flex_user_shortcodes(){
     vc_map(array(
